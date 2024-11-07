9ccfb4c5c048d90ab3516a42e9ada5daea93eb24
Nuha2222 <106200637+Nuha2222@users.noreply.github.com>
2024-11-06 22:26:31 -0500

    Add files via upload

---
+++ lib/pages/milestones.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:login/components/my_textfield.dart';
+import 'package:login/components/square_tile.dart';
+
+class petMilestones extends StatelessWidget {
+  petMilestones({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Milestones'),
+        backgroundColor: Colors.brown[300],
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: Colors.brown[100],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+
+            children: <Widget>[
+              // First notif
+              Text(
+                'Milestones',
+                style: TextStyle(
+                  color: Colors.grey[900],
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 60,
+                width: 400,
+              child: Card(
+                elevation: 20,
+                color: Colors.white,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      '🔔 Your pet has turned 2 years old!',
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+               ),
+              ),
+              ),
+
+
+              const SizedBox(height: 20), // Space between cards
+
+              // Second notif
+              Container(
+                height: 60,
+                width: 400,
+                child: Card(
+                  elevation: 20,
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        '🔔 Your pet is potty trained!',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+
+              const SizedBox(height: 20), // Space between cards
+
+              // Third notif
+              Container(
+                height: 60,
+                width: 400,
+                child: Card(
+                  elevation: 20,
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        '🔔 Your pet made a new friend!',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20), // Space between cards
+
+
+              // Fourth notif
+              Container(
+                height: 60,
+                width: 400,
+                child: Card(
+                  elevation: 20,
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        '🔔 Your pet lost their first tooth!',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+
+              const SizedBox(height: 20), // Space between cards
+
+              // Fifth notif
+              Container(
+                height: 60,
+                width: 400,
+                child: Card(
+                  elevation: 20,
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        '🔔 Your pet has turned 1 year old!',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/pet_checklist.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+
+class petChecklist extends StatefulWidget {
+  const petChecklist({super.key});
+
+  @override
+  _PetChecklistState createState() => _PetChecklistState();
+}
+
+class _PetChecklistState extends State<petChecklist> {
+  List<String> taskTitles = [
+    'Feed your pet',
+    'Groom your pet',
+    'Play with your pet',
+    'Complete a learning quiz',
+    'Play a mini game',
+  ];
+
+  late List<bool> tasks;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tasks = List.generate(taskTitles.length, (index) => false);
+
+    // Debugging: Print the lengths of both lists
+    print("taskTitles length: ${taskTitles.length}");
+    print("tasks length: ${tasks.length}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (taskTitles.length != tasks.length) {
+      print('Error: taskTitles and tasks lists are not of the same length.');
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pet Care Checklist'),
+        backgroundColor: Colors.brown[300],
+      ),
+      backgroundColor: Colors.brown[100],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Pet Care Checklist',
+                style: TextStyle(
+                  color: Colors.grey[900],
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: taskTitles.length,
+                  itemBuilder: (context, index) {
+
+                    return CheckboxListTile(
+                      title: Text(taskTitles[index]),
+                      value: tasks[index],
+                      onChanged: (bool? value) {
+                        setState(() {
+                          tasks[index] = value ?? false;
+                        });
+                      },
+                      activeColor: Colors.green,
+                      checkColor: Colors.white,
+                      tileColor: Colors.white,
+                      contentPadding: const EdgeInsets.all(8.0),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
