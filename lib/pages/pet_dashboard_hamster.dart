835b1bb458e37c1d734a0cee6e8b248592eef794
Nuha2222 <106200637+Nuha2222@users.noreply.github.com>
2024-11-11 09:56:56 -0500

    Update pet_dashboard_hamster.dart

--- lib/pages/pet_dashboard_hamster.dart
+++ lib/pages/pet_dashboard_hamster.dart
@@ -11,29 +11,35 @@ class petDashboardHamster extends StatefulWidget {
 class _PetDashboardState extends State<petDashboardHamster> {
   double moodLevel = 50.0;
   late Timer moodTimer;
+  int _selectedIndex = 0;
+
+  // Track the actions for each pet activity
+  bool isFeeding = false;
+  bool isGrooming = false;
+  bool isCuddling = false;
 
   @override
   void initState() {
     super.initState();
-    _startMoodDecrease(); // Start the mood decrease timer when the app starts
+    startMoodDecrease();
   }
 
-  // Start the timer to decrease mood every hour
-  void _startMoodDecrease() {
-    moodTimer = Timer.periodic(const Duration(hours: 1), (timer) {
-      _decreaseMood();
+  // Start the timer to decrease mood every 5 secs
+  void startMoodDecrease() {
+    moodTimer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      decreaseMood();
     });
   }
 
   // Function to decrease mood by 5% every time the timer ticks
-  void _decreaseMood() {
+  void decreaseMood() {
     setState(() {
       moodLevel = (moodLevel - (moodLevel * 0.05)).clamp(0.0, 100.0);
     });
   }
 
   // Update mood level when user interacts
-  void _updateMood(double moodChange) {
+  void updateMood(double moodChange) {
     setState(() {
       moodLevel = (moodLevel + moodChange).clamp(0.0, 100.0);
     });
@@ -45,6 +51,26 @@ class _PetDashboardState extends State<petDashboardHamster> {
     super.dispose();
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/petchecklist');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/milestones');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/pet_quizzes');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/login2');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,24 +78,32 @@ class _PetDashboardState extends State<petDashboardHamster> {
       body: SafeArea(
         child: Stack(
           children: [
+            new Container(
+              decoration: new BoxDecoration(
+                image: new DecorationImage(image: new AssetImage('lib/images/bg1.png'), fit: BoxFit.cover),
+              ),
+            ),
 
             Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
+                  const SizedBox(height: 20),
                   Text(
-                    'Pet Dashboard',
+                    'Pet Pals',
                     style: TextStyle(
                       color: Colors.grey[900],
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 20),
+
+                  const SizedBox(height: 150),
 
                   // Mood status bar
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
                     child: Column(
                       children: [
                         // Mood level label
@@ -77,7 +111,7 @@ class _PetDashboardState extends State<petDashboardHamster> {
                           'Mood Level: ${moodLevel.toInt()}%',
                           style: TextStyle(
                             fontSize: 18,
-                            color: moodLevel > 50 ? Colors.green : Colors.red,
+                            color: moodLevel > 50 ? Colors.green.shade900 : Colors.red.shade900,
                           ),
                         ),
                         const SizedBox(height: 10),
@@ -86,7 +120,7 @@ class _PetDashboardState extends State<petDashboardHamster> {
                         LinearProgressIndicator(
                           value: moodLevel / 100,
                           minHeight: 15,
-                          backgroundColor: Colors.grey[100],
+                          backgroundColor: Colors.white,
                           valueColor: AlwaysStoppedAnimation<Color>(
                             moodLevel > 50 ? Colors.green : Colors.red,
                           ),
@@ -95,31 +129,64 @@ class _PetDashboardState extends State<petDashboardHamster> {
                       ],
                     ),
                   ),
+
+                  // Row with buttons
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       ElevatedButton(
                         child: const Text("Feed pet"),
                         onPressed: () {
-                          _updateMood(20);
+                          setState(() {
+                            isFeeding = true;
+                            isGrooming = false;
+                            isCuddling = false;
+                          });
+                          updateMood(20);
+
+                          Future.delayed(const Duration(seconds: 3), () {
+                            setState(() {
+                              isFeeding = false;
+                            });
+                          });
                         },
                       ),
-
-                      SizedBox(width: 20),
-
+                      const SizedBox(width: 20),
                       ElevatedButton(
                         child: const Text("Groom pet"),
                         onPressed: () {
-                          _updateMood(20);
+                          setState(() {
+                            isFeeding = false;
+                            isGrooming = true;
+                            isCuddling = false;
+                          });
+                          updateMood(20);
+
+                          // Reset to false after 3 seconds (optional)
+                          Future.delayed(const Duration(seconds: 3), () {
+                            setState(() {
+                              isGrooming = false;
+                            });
+                          });
                         },
                       ),
-
-                      SizedBox(width: 20),
-
+                      const SizedBox(width: 20),
                       ElevatedButton(
                         child: const Text("Cuddle pet"),
                         onPressed: () {
-                          _updateMood(20);
+                          setState(() {
+                            isFeeding = false;
+                            isGrooming = false;
+                            isCuddling = true;
+                          });
+                          updateMood(20);
+
+
+                          Future.delayed(const Duration(seconds: 3), () {
+                            setState(() {
+                              isCuddling = false;
+                            });
+                          });
                         },
                       ),
                     ],
@@ -127,40 +194,21 @@ class _PetDashboardState extends State<petDashboardHamster> {
 
                   const SizedBox(height: 50),
 
-                  Card(
-                    elevation: 20,
-                    color: Colors.white,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
 
-                        Image.asset(
-                          'lib/images/cat.png',
-                          height: 200,
-                          width: 200,
-                          fit: BoxFit.fill,
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  ElevatedButton(
-                    child: const Text("Pet checklist"),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/petchecklist');
-                    },
-                  ),
-
-                  ElevatedButton(
-                    child: const Text("Milestones"),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/milestones');
-                    },
+                  Container(
+                    width: 300,
+                    height: 300,
+                    child: isFeeding
+                        ? Image.asset('lib/images/hamster.png', fit: BoxFit.cover) // Show feeding image
+                        : isGrooming
+                        ? Image.asset('lib/images/hamster.png', fit: BoxFit.cover) // Show grooming image
+                        : isCuddling
+                        ? Image.asset('lib/images/hamster.png', fit: BoxFit.cover) // Show cuddling image
+                        : Image.asset('lib/images/hamster.png', fit: BoxFit.cover), // Default cat image
                   ),
                 ],
               ),
             ),
-
 
             // Profile pic
             Positioned(
@@ -190,9 +238,41 @@ class _PetDashboardState extends State<petDashboardHamster> {
               ),
             ),
           ],
-
         ),
       ),
+
+      // Bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('lib/images/checklist.png', height: 30, width: 30),
+            label: 'Checklist',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('lib/images/milestones.png', height: 30, width: 30),
+            label: 'Milestones',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('lib/images/quizzes.png', height: 30, width: 30),
+            label: 'Quizzes',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('lib/images/logout.png', height: 30, width: 30),
+            label: 'Logout',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(color: Colors.black),
+        unselectedLabelStyle: TextStyle(color: Colors.black),
+      ),
+
     );
   }
 }
